c7ecada467efbafad2b7d5c875f910db0b6b0dfd
Juan Echeverria <juan.echeverriatro@ug.edu.ec>
2025-01-12 14:27:07 -0500

    Endpoint para devolver acciones y entidades de la bitacora

--- src/controllers/bitacoraController.ts
+++ src/controllers/bitacoraController.ts
@@ -3,18 +3,24 @@ import { Usuario } from '../models/usuarioModel';
 import { handleHttp } from '../utils/handleError';
 import { Rol } from '../models/rolModel';
 import { Bitacora } from '../models/bitacoraModel';
+import { col, fn, Op } from 'sequelize';
 
 const getBitacora = async (req: Request, res: Response) => {
+    const { fechaInicio, fechaFin } = req.body;
     try {
         const bitacora = await Bitacora.findAll({
-            include: [{
+            where: {
+                fecha: {
+                    [Op.between]: [fechaInicio, fechaFin],
+                },
+            }, include: [{
                 model: Usuario,
                 as: 'usuario',
                 attributes: ['nombre'],
                 include: [{
                     model: Rol,
                     as: 'rol',
-                    attributes: ['idRol','nombre']
+                    attributes: ['idRol', 'nombre']
                 }]
             }],
             order: [['fecha', 'DESC']]
@@ -28,4 +34,42 @@ const getBitacora = async (req: Request, res: Response) => {
     }
 };
 
-export{ getBitacora }
+const getAcciones = async (req: Request, res: Response) => {
+    try {
+        const acciones = await Bitacora.findAll({
+            attributes: [
+              'accion',
+              [fn('COUNT', col('accion')), 'total'] // Cuenta cuántas veces aparece cada acción
+            ],
+            group: ['accion'], // Agrupa por el campo 'accion'
+            order: [['accion', 'ASC']],
+          });
+        res.status(200).json({
+            status: true,
+            value: acciones
+        });
+    } catch (error) {
+        handleHttp(res, 'ERROR_GET_ALL', error);
+    }
+};
+
+const getEntidades = async (req: Request, res: Response) => {
+    try {
+        const entidades = await Bitacora.findAll({
+            attributes: [
+              'entidad',
+              [fn('COUNT', col('entidad')), 'total'] // Cuenta cuántas veces aparece cada acción
+            ],
+            group: ['entidad'], // Agrupa por el campo 'accion'
+            order: [['entidad', 'ASC']],
+          });
+        res.status(200).json({
+            status: true,
+            value: entidades
+        });
+    } catch (error) {
+        handleHttp(res, 'ERROR_GET_ALL', error);
+    }
+};
+
+export { getBitacora, getAcciones, getEntidades }
--- src/routes/bitacoraRoutes.ts
+++ src/routes/bitacoraRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { getBitacora } from '../controllers/bitacoraController';
+import { getBitacora, getAcciones, getEntidades } from '../controllers/bitacoraController';
 import { checkJwt } from '../middlewares/session';
 
 const router = Router();
 
-router.get('/', checkJwt, getBitacora);
+router.get('/acciones', checkJwt, getAcciones);
+router.get('/entidades', checkJwt, getEntidades);
+router.post('/', checkJwt, getBitacora);
 
 export default router;
