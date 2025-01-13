f9d926e673a06e84ec1804ac40aa147c75de4590
Juan Echeverria <juan.echeverriatro@ug.edu.ec>
2025-01-12 23:17:59 -0500

    Borrar directorio dist

--- dist/routes/stockRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const stockController_1 = require("../controllers/stockController");
-const router = (0, express_1.Router)();
-router.get('/', stockController_1.getStock);
-router.get('/producto/:idP/:idU', stockController_1.getStockProductoByUbicacion);
-exports.default = router;
