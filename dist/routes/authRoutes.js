feacc5428f0e3945ec82b9e37cc7f8f0f3969482
Juan Echeverria <juan.echeverriatro@ug.edu.ec>
2025-01-15 13:27:08 -0500

    Actualizacion de cors

--- dist/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const authController_1 = require("../controllers/authController");
-const router = (0, express_1.Router)();
-router.post('/', authController_1.login);
-router.post('/change', authController_1.changePassword);
-router.post('/forgot', authController_1.forgotPassword);
-exports.default = router;
--- src/app.ts
+++ src/app.ts
@@ -8,9 +8,7 @@ const PORT = process.env.PORT;
 const app = express();
 
 connectDB();
-app.use(cors({
-    origin: "http://54.172.159.35"
-}));
+app.use(cors());
 app.use(express.json())
 app.use(router);
 app.listen(PORT, () => console.log("Connected on port " + PORT));
