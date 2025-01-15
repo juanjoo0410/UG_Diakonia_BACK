475ae75bafc445213d9165906d1e97a06f0e1a31
Juan Echeverria <juan.echeverriatro@ug.edu.ec>
2025-01-15 18:27:46 -0500

    Prueba 6 de despliegue

--- dist/routes/kardexRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const kardexController_1 = require("../controllers/kardexController");
-const session_1 = require("../middlewares/session");
-const router = (0, express_1.Router)();
-router.post('/', session_1.checkJwt, kardexController_1.getKardex);
-exports.default = router;
