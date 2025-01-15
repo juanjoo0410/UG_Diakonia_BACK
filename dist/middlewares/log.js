1cfd07276619bc3a963a4d1e53acb13be78719c5
Juan Echeverria <juan.echeverriatro@ug.edu.ec>
2025-01-15 12:42:07 -0500

    Compilacion actualizada 20250115

--- dist/middlewares/log.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.log = void 0;
-const log = (req, res, next) => {
-    const userAgent = req.headers['user-agent'];
-    console.log('Accediendo desde ' + userAgent);
-    next();
-};
-exports.log = log;
