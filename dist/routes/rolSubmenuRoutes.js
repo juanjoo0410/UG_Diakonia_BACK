8000a05bb546af02eebb6fc09aee21dbea243243
Juan Echeverria <juan.echeverriatro@ug.edu.ec>
2025-01-15 17:05:14 -0500

    Ignorar dist

--- dist/routes/rolSubmenuRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const rolSubmenuController_1 = require("../controllers/rolSubmenuController");
-const router = (0, express_1.Router)();
-router.get('/:idRol', rolSubmenuController_1.getPermisosByIdRol);
-router.get('/idSubmenus/:idRol', rolSubmenuController_1.getIdSubmenusByIdRol);
-exports.default = router;
